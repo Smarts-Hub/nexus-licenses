5772e40b871611045ffa3a18d6c67913a015b548
jallox <javiluky2009@gmail.com>
2024-10-17 07:21:27 +0200

    Implement product creation at API

--- src/server/routes/api.routes.js
+++ src/server/routes/api.routes.js
@@ -2,7 +2,10 @@ import express from "express";
 const router = express.Router();
 import config from "../../config.js"
 import { decrypt } from "../../utils.js"
-import { checkLicense, validateLicense } from "../controllers/licenses.controller.js";
+
+import licenses from "../../schemas/Licenses.js"
+import Products from "../../schemas/Products.js";
+
 router.get("/", async (req, res) => {
   res.json({
     status: "ok",
@@ -21,8 +24,36 @@ router.get("/check", async (req, res) => {
     return res.json({ status: "error", message: "Invalid token", data: { error: 'Invalid token'} });
   }
 
-  res.json({ status: "ok", message: "Valid token"});
+  res.json({ status: "ok", message: "Valid token", data: { permissions: "allow_all", token: token, timestamp: Date.now() } });
 });
+
+router.post('/products/create', async (req, res) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.json({ status: "error", message: "Invalid token", data: { error: 'Undefined token'} });
+  }
+
+  if (token !== await decrypt(config.interactionApiKey)) {
+    return res.json({ status: "error", message: "Invalid token", data: { error: 'Invalid token'} });
+  }
+
+  const product = req.body.name;
+  const roleID = req.body.roleID;
+  
+  if(!product || !roleID) {
+    return res.json({ status: "error", message: "Invalid request", data: { error: 'Missing required fields'} });
+  }
+  if(await Products.findOne({ name: product })) {
+    return res.json({ status: "error", message: "Product already exists", data: { error: 'Product already exists'} });
+  }
+
+  const newProduct = new Products({ product, customerRoleId: roleID });
+  return res.json({ status: "success", message: "Product created", data: { product: newProduct, timestamp: Date.now() } });
+})
+
+router.get('/products/list', async (req, res) => {
+  
+})
 
 
 export default router;
